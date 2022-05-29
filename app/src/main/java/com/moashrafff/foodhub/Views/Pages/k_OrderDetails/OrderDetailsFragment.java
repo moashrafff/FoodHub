61bc81ace4eb33055a56594df6b656f8ab66772a
unknown <mohamed.ashraf.rady0@gmail.com>
2022-05-29 04:02:11 +0200

    order details implementation

---
+++ app/src/main/java/com/moashrafff/foodhub/Views/Pages/k_OrderDetails/OrderDetailsFragment.java
@@ -0,0 +1,45 @@
+package com.moashrafff.foodhub.Views.Pages.k_OrderDetails;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.moashrafff.foodhub.R;
+import com.moashrafff.foodhub.databinding.FragmentOrderDetailsBinding;
+
+
+public class OrderDetailsFragment extends Fragment implements View.OnClickListener {
+
+    private FragmentOrderDetailsBinding binding;
+
+    public OrderDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentOrderDetailsBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
