b6affa565b6abedc88fd4098d9f897d2b92917b5
unknown <mohamed.ashraf.rady0@gmail.com>
2022-05-29 18:55:19 +0200

    filter implementation

---
+++ app/src/main/java/com/moashrafff/foodhub/Views/Pages/e_Search/FilterFragment.java
@@ -0,0 +1,39 @@
+package com.moashrafff.foodhub.Views.Pages.e_Search;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.moashrafff.foodhub.R;
+import com.moashrafff.foodhub.databinding.FragmentFilterBinding;
+
+public class FilterFragment extends Fragment {
+
+    private FragmentFilterBinding binding;
+
+    public FilterFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentFilterBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
